47f58fe8871f2a1674c2658cb0704016adf60cb4
Hui <wang0x20hui@gmail.com>
2019-12-07 21:57:01 +0800

    Add cmake config

--- main.cpp
+++ main.cpp
@@ -1,6 +1,18 @@
-#include <iostream>
+#include "itkImage.h"
+#include "itkImageFileReader.h"
+#include "itkGradientMagnitudeImageFilter.h"
 
-int main() {
-    std::cout << "Hello, World!" << std::endl;
+int main( int argc, char **argv ) {
+    typedef itk::Image<unsigned short,2>    	  ImageType;
+    typedef itk::ImageFileReader<ImageType> 	  ReaderType;
+    typedef itk::GradientMagnitudeImageFilter<
+            ImageType,ImageType>     FilterType;
+
+    ReaderType::Pointer reader = ReaderType::New();
+    FilterType::Pointer filter = FilterType::New();
+
+    reader->SetFileName( argv[1] );
+    filter->SetInput( reader->GetOutput() );
+    filter->Update();
     return 0;
 }
